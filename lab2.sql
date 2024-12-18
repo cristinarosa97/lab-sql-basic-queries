f159a1acd5d468e637cb7b97d7d1d19b1dce7110
cristinarosa97 <cristinarosaquero@gmail.com>
2024-12-18 15:03:37 +0100

    w4 lab2 done

--- lab2.sql
+++ lab2.sql
@@ -9,8 +9,8 @@ SELECT first_name FROM sakila.staff;
 #4
 SELECT DISTINCT release_year FROM sakila.film;
 #5
-SELECT COUNT(store_id) FROM sakila.store;
-SELECT COUNT(staff_id) FROM sakila.staff;
+SELECT COUNT(DISTINCT store_id) FROM sakila.store;
+SELECT COUNT(DISTINCT staff_id) FROM sakila.staff;
 SELECT COUNT(DISTINCT inventory_id) FROM sakila.rental;
 SELECT SUM(isnull(return_date)) as "not returned" FROM sakila.rental;
 SELECT DISTINCT last_name FROM sakila.actor;
@@ -24,5 +24,5 @@ WHERE first_name = "Scarlett";
 #BONUS
 SELECT * FROM sakila.film
 WHERE title LIKE '%ARMAGEDDON%' and length > 100;
-SELECT * FROM sakila.film
+SELECT COUNT(*) FROM sakila.film
 WHERE special_features = "behind the scenes";
